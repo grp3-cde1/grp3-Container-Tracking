fbc98782b9c545045078caa70cfbb031c0daff6e
Lou Brauchli <lou.brauchli@gmail.com>
2026-03-19 14:47:23 +0100

    added csv import

--- apps/retrospective_app.py
+++ apps/retrospective_app.py
@@ -1,1 +1,5 @@
+import pandas as pd
 
+track_df = pd.read_csv('grp3-Container-Tracking/data/luzern-horw.csv', header=None, names=["timestamp", "latitude", "longitude", "temperature", "humidity"], index_col="timestamp")
+
+print(track_df['temperature'].median())
