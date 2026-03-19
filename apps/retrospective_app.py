8bfd4f42bfb19c57ce0ec78e4ff213cc4a0257c3
Lou Brauchli <lou.brauchli@gmail.com>
2026-03-19 15:55:22 +0100

    added http get for container list

--- apps/retrospective_app.py
+++ apps/retrospective_app.py
@@ -1,5 +1,23 @@
-import pandas as pd
+# import pandas as pd
 
-track_df = pd.read_csv('grp3-Container-Tracking/data/luzern-horw.csv', header=None, names=["timestamp", "latitude", "longitude", "temperature", "humidity"], index_col="timestamp")
+# track_df = pd.read_csv('grp3-Container-Tracking/data/luzern-horw.csv', header=None, names=["timestamp", "latitude", "longitude", "temperature", "humidity"], index_col="timestamp")
 
-print(track_df['temperature'].median())
+# print(track_df['temperature'].median())
+
+import requests
+
+container_id = "gpr3"
+
+route_id = "luzern-horw"
+
+url = 'https://fl-17-240.zhdk.cloud.switch.ch/'
+
+response = requests.get(f"{url}containers")
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+    print(data["containers"])
+
+else:
+    print("Fehler: ", response.status_code)
