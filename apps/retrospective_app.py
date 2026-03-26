a21460ecf1b8bf69480a77bc359e19d2641a9a67
Lou Brauchli <lou.brauchli@gmail.com>
2026-03-26 09:32:13 +0100

    added http requests and created menu to choose container and route of the container

--- apps/retrospective_app.py
+++ apps/retrospective_app.py
@@ -12,12 +12,44 @@ route_id = "luzern-horw"
 
 url = 'https://fl-17-240.zhdk.cloud.switch.ch/'
 
-response = requests.get(f"{url}containers")
+response_container = requests.get(f"{url}containers")
 
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-    print(data["containers"])
+if response_container.status_code == 200:
+    data_container = response_container.json()
+    # print(data)
+    listed_containers = data_container["containers"]
+
+    print("\nAuswahl Container-Tracking:\n------------------")
+    for container in listed_containers:
+        print(container)
+
+    chosen_container = input("Wähle einen Container aus: ")
+
+    if chosen_container in listed_containers:
+        print("True")
+    else:
+        print("Bitte wähle einen Container aus dem Menü aus.")
 
 else:
-    print("Fehler: ", response.status_code)
+    print("Fehler: ", response_container.status_code)
+
+response_route = requests.get(f"{url}containers/{chosen_container}/routes")
+
+if response_route.status_code == 200:
+    data_route = response_route.json()
+    print(data_route)
+    listed_routes = data_route["routes"]
+
+    print("\nAuswahl Route des Containers:\n------------------")
+    for route in listed_routes:
+        print(route)
+
+    chosen_route = input("Wähle eine Route aus: ")
+
+    if chosen_route in listed_routes:
+        print("True")
+    else:
+        print("Bitte wähle eine Route aus dem Menü aus.")
+
+else:
+    print("Fehler: ", response_route.status_code)
